c486b89c7ce805493e0230dd52932de8d56ab5f3
nikhilkamathn <38173438+nikhilkamathn@users.noreply.github.com>
2023-09-13 13:56:09 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,29 @@
-#include <stdio.h>
+// Test file to show bug in 1.27
+
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
+
 int main()
 {
-    printf("hello world\n");
-  
-  return 1; 
+	FILE	*f;
+	char	buf[1024], *s;
+	int	first;
+
+	first = 1;
+	while(fgets(buf, sizeof(buf), stdin) != 0) {
+		if(first == 0) {
+			printf("\n");
+		}
+		s = buf;
+		while(*s != '\0') {
+			if(*s == '\n' || *s == '\r') {
+				*s = '\0';
+				break;
+			}
+			s++;
+		}
+		printf("%s", buf);
+		first = 0;
+	}
 }
